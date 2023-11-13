c9aec59740923697f7dadff9cf17a9982924f476
Muhammed Ali LEVENT <147024939+mlpersonel@users.noreply.github.com>
2023-11-13 14:09:38 +0300

    initialCommit
    init

---
+++ app/src/main/java/com/levent/studycase/movies/MoviesFragment.kt
@@ -0,0 +1,45 @@
+package com.levent.studycase.movies
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.levent.studycase.R
+import com.levent.studycase.databinding.FragmentMoviesBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MoviesFragment : Fragment() {
+
+    private var _binding: FragmentMoviesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
